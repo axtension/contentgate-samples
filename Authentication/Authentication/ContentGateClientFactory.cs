5b328e22a86674d306f89450b85e89425186a927
Jeroen Daniels <Jeroen.Daniels@axtension.com>
2021-01-07 10:26:28 +0100

    Add Authentication solution and sample code

---
+++ Authentication/Authentication/ContentGateClientFactory.cs
@@ -0,0 +1,43 @@
+﻿using AXtension.ContentGate.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentication
+{
+    class ContentGateClientFactory
+    {
+        private readonly Uri _baseUrl;
+        private readonly string _clientId;
+        private readonly string _authority;
+
+        public ContentGateClientFactory(Uri baseUrl, string clientId, string authority)
+        {
+            this._baseUrl = baseUrl;
+            this._clientId = clientId;
+            this._authority = authority;
+        }
+
+        public ContentGateClient GetClientWithDeviceCode()
+        {
+            // for device authentication, simply use the default console output to instruct the user.
+            // how to authenticate.
+            var deviceCodeAuthenticator = DeviceAuthentication.Console;
+
+            // construct the client.
+            return new ContentGateClient(_baseUrl, new ContentGateCredentials(_clientId, _authority, deviceCodeAuthenticator));
+        }
+
+        public ContentGateClient GetClientWithClientIdAndSecret(string clientSecret)
+            => new ContentGateClient(_baseUrl, new ContentGateCredentials(_clientId, _authority, clientSecret));
+        
+        public ContentGateClient GetClientWithUsernameAndPassword(string username, string password)
+            => new ContentGateClient(_baseUrl, new ContentGateCredentials(_clientId, _authority, username, password));
+
+        public ContentGateClient GetClientWithUserPrompt()
+            => new ContentGateClient(_baseUrl, new ContentGateCredentials(_clientId, _authority));
+
+        public ContentGateClient GetClientWithCustomAuthorizationHeader()
+            => new ContentGateClient(_baseUrl, new CustomContentGateCredentials(_clientId, _authority));
+    }
+}
---
+++ Authentication/Authentication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Authentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // the base uri that references the Content Gate tenant.
+            var baseUrl = new Uri("## TENANT ##"); // fill in the tenant. (e.g. https://axtension.content-gate.com)
+
+            // the client id of the app registered in the tenant.
+            var clientId = "## CLIENT ID##"; // fill in the client id here. (e.g. 1bd5690d-2902-4400-b7bb-d292691e6323)
+
+            // the authority that ContentGate belongs to, and accepts signed tokens of.
+            var authority = "## AUTHORITY ##"; // fill in the authority here. (e.g. https://login.microsoftonline.com/axtension.com)
+
+            try
+            {
+                // Create a client factory object.
+                var clientFactory = new ContentGateClientFactory(baseUrl, clientId, authority);
+
+                // Create the client.
+                var contentGateClient = clientFactory.GetClientWithUserPrompt();
+            }
+            catch (AggregateException ag)
+            {
+                if (ag.InnerExceptions.Count == 1)
+                    throw ag.InnerException;
+                else
+                    throw ag.Flatten();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
